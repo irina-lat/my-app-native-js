72d34f35a788da34bd0d4ed3e6b92d3121bafccb
irina-lat <irochka.bars@mail.ru>
2022-07-06 15:21:14 +0300

    first commit

---
+++ src/01-test/01.test.ts
@@ -0,0 +1,58 @@
+import {mult, splitIntoWord, sum} from "./01";
+
+test('sum shold be correct', () => {
+        //data
+        let a;
+        let b;
+        let c;
+
+        beforeEach(()=>{
+            a=1;
+            b=2;
+            c=3;
+    })
+
+        //action
+        const result = sum(a, b);
+        a=100;
+        const result2 = sum(b, c)
+
+        //expect result
+        expect(result).toBe(3)
+        expect(result2).toBe(102)
+    }
+)
+
+test('multiply should be correct', () => {
+        //data
+        const a = 1;
+        const b = 2;
+        const c = 3;
+
+        //action
+        const result1 = mult(a, b)
+        const result2 = mult(b, c)
+
+        //expect result
+        expect(result1).toBe(2)
+        expect(result2).toBe(6)
+    }
+)
+
+// test('spliting into word shold be correct', () => {
+//     //data
+//     const sent1 = "Hello my friend!";
+//     //
+//
+//     //action
+//     const result1 = splitIntoWord(sent1)
+//     //
+//
+//     //expect result
+//     expect(result1.length).toBe(3)
+//     expect(result1[0]).toBe("hello")
+//     expect(result1[1]).toBe("my")
+//     expect(result1[2]).toBe("frined")
+// })
+//
+//
---
+++ src/01-test/01.ts
@@ -0,0 +1,12 @@
+export function sum(a: number, b: number) {
+    return (a + b)
+}
+
+export function mult(a: number, b: number) {
+    return (a * b)
+}
+
+// export function splitIntoWord(sentense: string) {
+//     return sentense.toLowerCase().split("")
+//         .map(w => w.replace("!", ""))
+// }
